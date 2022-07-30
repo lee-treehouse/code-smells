2c43ac76102c2de3ec7d43441ceecd18278cc40c
Lauro Fialho Müller <contact@lauromueller.com>
2022-07-30 11:02:53 +0200

    feat(primitive-obsession): solution to code smell

--- src/primitive-obsession/index.ts
+++ src/primitive-obsession/index.ts
@@ -1,37 +1,84 @@
 import { v4 as uuidV4 } from "uuid";
 
+type CustomerRawData = {
+  name: string;
+  address: string;
+  phone: string;
+  email: string;
+  id?: string;
+};
+
+class Email {
+  private _emailValue: string;
+  private readonly EMAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  constructor(emailValue: string) {
+    this._emailValue = emailValue;
+  }
+
+  isValid() {
+    return this.EMAIL_REGEX.test(this._emailValue);
+  }
+}
+
+class Phone {
+  private _areaCode: string;
+  private _phoneNumber: string;
+
+  constructor(phoneValue: string) {
+    const phoneComponents = phoneValue.split(" ");
+    this._areaCode = phoneComponents[0];
+    this._phoneNumber = phoneComponents[1];
+  }
+
+  isValid() {
+    return this._areaCode.length === 2 && this._phoneNumber.length === 8;
+  }
+
+  getAreaCode() {
+    return this._areaCode;
+  }
+
+  getNumber() {
+    return this._phoneNumber;
+  }
+}
+
 class Customer {
   private _id: string;
   private _name: string;
   private _address: string;
-  private _phone: string;
-  private _email: string;
-  private readonly EMAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  private readonly PHONE_REGEX = /\d{2}\ \d{8}/;
+  private _phone: Phone;
+  private _email: Email;
 
-  constructor(name: string, address: string, phone: string, email: string, id?: string) {
+  constructor(customerData: CustomerRawData) {
+    const { name, address, phone, email, id } = customerData;
+
     this._id = id || uuidV4();
     this._name = name;
     this._address = address;
-    this._phone = phone;
-    this._email = email;
+    this._phone = new Phone(phone);
+    this._email = new Email(email);
   }
 
-  isEmailValid() {
-    return this.EMAIL_REGEX.test(this._email);
+  getEmail() {
+    return this._email;
   }
 
-  isPhoneValid() {
-    return this.PHONE_REGEX.test(this._phone);
-  }
-
-  getPhoneAreaCode() {
-    return this._phone.split(" ")[0];
-  }
-
-  getPhoneNumber() {
-    return this._phone.split(" ")[1];
+  getPhone() {
+    return this._phone;
   }
 }
+
+const myCustomer = new Customer({
+  name: "Lauro",
+  address: "Street 123",
+  phone: "12 34567890",
+  email: "lauro@example.com",
+});
+
+console.log(myCustomer.getEmail().isValid());
+console.log(myCustomer.getPhone().isValid());
+console.log(myCustomer.getPhone().getAreaCode());
 
 export {};
