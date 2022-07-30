23bad92b4f404eb94c5e7637578308a09828d31f
Lauro Fialho Müller <contact@lauromueller.com>
2022-07-30 15:45:40 +0200

    feat(divergent-change): solution to code smell

--- src/divergent-change/index.ts
+++ src/divergent-change/index.ts
@@ -20,16 +20,25 @@ interface Return {
   getReturnedProducts(): Product[];
 }
 
-interface OrderManager {
+interface OrderService {
   createOrder: (customerId: string, products: Product[]) => Order;
   deleteOrder: (orderId: string) => void;
   addProductsToOrder: (orderId: string, products: Product[]) => void;
   removeProductsFromOrder: (orderId: string, products: Product[]) => void;
   getOrderDetails: (orderId: string) => Order;
+}
+
+interface CustomerService {
   registerCustomerAddress: (customerId: string, address: string) => void;
   getCustomerAddress: (customerId: string) => string;
+}
+
+interface ShipmentService {
   setShipmentAddress: (orderId: string, address: string) => void;
   getShipmentAddress: (orderId: string) => string;
+}
+
+interface ReturnService {
   initiateOrderReturn: (orderId: string, returnedProducts: Product[]) => Return;
   cancelReturn: (returnId: string) => void;
   getReturnStatus: (returnId: string) => string;
