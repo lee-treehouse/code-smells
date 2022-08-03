02935aed3a75809a1f83f0af8d95b7c964a58d11
Lauro Fialho Müller <contact@lauromueller.com>
2022-08-03 19:56:51 +0200

    feat(data-clumps): solution to code smell

--- src/data-clumps/index.ts
+++ src/data-clumps/index.ts
@@ -1,16 +1,51 @@
+type DimensionsRawData = {
+  width: number;
+  height: number;
+  depth: number;
+  unit?: "m" | "cm";
+};
+
+class Dimensions {
+  private _width: number;
+  private _height: number;
+  private _depth: number;
+  private _unit: "m" | "cm";
+
+  constructor({ width, height, depth, unit = "cm" }: DimensionsRawData) {
+    this._width = width;
+    this._height = height;
+    this._depth = depth;
+    this._unit = unit;
+  }
+
+  getWidth() {
+    return this._width;
+  }
+
+  getHeight() {
+    return this._height;
+  }
+
+  getDepth() {
+    return this._depth;
+  }
+}
+
+type ProductRawData = {
+  id: string;
+  description: string;
+  dimensions: Dimensions;
+};
+
 class Product {
   private _id: string;
   private _description: string;
-  private _boxWidth: number;
-  private _boxHeight: number;
-  private _boxDepth: number;
+  private _dimensions: Dimensions;
 
-  constructor(id: string, desc: string, boxWidth: number, boxHeight: number, boxDepth: number) {
+  constructor({ id, description, dimensions }: ProductRawData) {
     this._id = id;
-    this._description = desc;
-    this._boxWidth = boxWidth;
-    this._boxHeight = boxHeight;
-    this._boxDepth = boxDepth;
+    this._description = description;
+    this._dimensions = dimensions;
   }
 
   getIdentifier() {
@@ -19,15 +54,19 @@ class Product {
   getDescription() {
     return this._description;
   }
-  getBoxWidth() {
-    return this._boxWidth;
-  }
-  getBoxHeight() {
-    return this._boxHeight;
-  }
-  getBoxDepth() {
-    return this._boxDepth;
+  getBoxDimensions() {
+    return this._dimensions;
   }
 }
+
+new Product({
+  id: "123",
+  description: "A small product",
+  dimensions: new Dimensions({
+    width: 10,
+    height: 10,
+    depth: 10,
+  }),
+});
 
 export {};
