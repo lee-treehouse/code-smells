94232eec7d14eab9acda05c37c30fa9b5aeabf68
Lauro Fialho Müller <contact@lauromueller.com>
2022-08-03 17:09:34 +0200

    feat(long-parameter-list): solution to code smell

--- src/long-parameter-list/index.ts
+++ src/long-parameter-list/index.ts
@@ -1,28 +1,34 @@
-interface Customer {
+interface ICustomer {
   getIdentifier(): string;
   getAddress(): string;
   hasPremiumSubscription(): boolean;
 }
 
-class Order {
-  private _id: string;
+type OrderRawData = {
+  id?: string;
+  customer: ICustomer;
+  productIds: string[];
+};
+
+interface IOrder {
+  getProductIds(): string[];
+  getCustomerId(): string;
+  getDeliveryDays(): number;
+}
+
+class StandardOrder implements IOrder {
+  private _id: string | undefined;
   private _customerId: string;
   private _customerAddress: string;
-  private _hasPriority: boolean;
   private _productIds: string[];
 
-  constructor(
-    id: string,
-    customerId: string,
-    customerAddress: string,
-    productIds: string[],
-    isPremiumCustomer: boolean
-  ) {
+  constructor(orderData: OrderRawData) {
+    const { customer, id, productIds } = orderData;
+
     this._id = id;
-    this._customerId = customerId;
-    this._customerAddress = customerAddress;
+    this._customerId = customer.getIdentifier();
+    this._customerAddress = customer.getAddress();
     this._productIds = productIds;
-    this._hasPriority = isPremiumCustomer;
   }
 
   getProductIds() {
@@ -32,6 +38,84 @@ class Order {
   getCustomerId() {
     return this._customerId;
   }
+
+  getDeliveryDays() {
+    return 3;
+  }
 }
+
+class PremiumOrder implements IOrder {
+  private _id: string | undefined;
+  private _customerId: string;
+  private _customerAddress: string;
+  private _productIds: string[];
+
+  constructor(orderData: OrderRawData) {
+    const { customer, id, productIds } = orderData;
+
+    this._id = id;
+    this._customerId = customer.getIdentifier();
+    this._customerAddress = customer.getAddress();
+    this._productIds = productIds;
+  }
+
+  getProductIds() {
+    return this._productIds;
+  }
+
+  getCustomerId() {
+    return this._customerId;
+  }
+
+  getDeliveryDays() {
+    return 1;
+  }
+}
+
+const printDeliveryDays = (order: IOrder) => console.log(order.getDeliveryDays());
+
+// Priority orders have 1 day delivery
+class Customer implements ICustomer {
+  private _subscriptionType: "premium" | "standard";
+
+  constructor(subscriptionType: "premium" | "standard") {
+    this._subscriptionType = subscriptionType;
+  }
+
+  getAddress(): string {
+    return "";
+  }
+
+  getIdentifier(): string {
+    return "";
+  }
+
+  hasPremiumSubscription(): boolean {
+    return this._subscriptionType === "premium";
+  }
+}
+
+const orderFactory = (orderData: OrderRawData): IOrder => {
+  if (orderData.customer.hasPremiumSubscription()) {
+    return new PremiumOrder(orderData);
+  } else {
+    return new StandardOrder(orderData);
+  }
+};
+
+const standard = orderFactory({
+  id: "123",
+  customer: new Customer("standard"),
+  productIds: [],
+});
+
+const premium = orderFactory({
+  id: "123",
+  customer: new Customer("premium"),
+  productIds: [],
+});
+
+printDeliveryDays(standard);
+printDeliveryDays(premium);
 
 export {};
