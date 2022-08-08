3611dc0a3154896e1b3067cbb002edceeca4b615
Lauro Fialho Müller <contact@lauromueller.com>
2022-08-08 17:21:43 +0200

    feat(repeated-switches): solution to code smell

--- src/repeated-switches/index.ts
+++ src/repeated-switches/index.ts
@@ -1,63 +1,128 @@
+interface Operation {
+  operate(): number;
+  toString(): string;
+}
+
+class Add implements Operation {
+  private _oldValue: number;
+  private _newValue: number;
+
+  constructor(oldValue: number, newValue: number) {
+    this._oldValue = oldValue;
+    this._newValue = newValue;
+  }
+
+  operate(): number {
+    return this._oldValue + this._newValue;
+  }
+
+  toString(): string {
+    return `${this._oldValue} plus ${this._newValue}`;
+  }
+}
+
+class Subtract implements Operation {
+  private _oldValue: number;
+  private _newValue: number;
+
+  constructor(oldValue: number, newValue: number) {
+    this._oldValue = oldValue;
+    this._newValue = newValue;
+  }
+
+  operate(): number {
+    return this._oldValue - this._newValue;
+  }
+
+  toString(): string {
+    return `${this._oldValue} minus ${this._newValue}`;
+  }
+}
+
+class Multiply implements Operation {
+  private _oldValue: number;
+  private _newValue: number;
+
+  constructor(oldValue: number, newValue: number) {
+    this._oldValue = oldValue;
+    this._newValue = newValue;
+  }
+
+  operate(): number {
+    return this._oldValue * this._newValue;
+  }
+
+  toString(): string {
+    return `${this._oldValue} multiplied by ${this._newValue}`;
+  }
+}
+
+class Divide implements Operation {
+  private _oldValue: number;
+  private _newValue: number;
+
+  constructor(oldValue: number, newValue: number) {
+    this._oldValue = oldValue;
+    this._newValue = newValue;
+  }
+
+  operate(): number {
+    return this._oldValue / this._newValue;
+  }
+
+  toString(): string {
+    return `${this._oldValue} divided by ${this._newValue}`;
+  }
+}
+
 class Calculator {
-  private _operations: { operation: string; oldValue: number; value: number }[] = [];
+  private _operations: Operation[] = [];
   private _currentValue: number;
 
   constructor(initialValue: number) {
     this._currentValue = initialValue;
   }
 
-  execute(operation: string, newValue: number) {
+  private createOperation(operation: string, newValue: number): Operation {
     switch (operation) {
       case "add":
-        this._operations.push({ operation: "add", oldValue: this._currentValue, value: newValue });
-        this._currentValue = this._currentValue + newValue;
-        return this;
+        return new Add(this._currentValue, newValue);
       case "subtract":
-        this._operations.push({
-          operation: "subtract",
-          oldValue: this._currentValue,
-          value: newValue,
-        });
-        this._currentValue = this._currentValue - newValue;
-        return this;
+        return new Subtract(this._currentValue, newValue);
       case "multiply":
-        this._operations.push({
-          operation: "multiply",
-          oldValue: this._currentValue,
-          value: newValue,
-        });
-        this._currentValue = this._currentValue * newValue;
-        return this;
+        return new Multiply(this._currentValue, newValue);
       case "divide":
-        this._operations.push({
-          operation: "divide",
-          oldValue: this._currentValue,
-          value: newValue,
-        });
-        this._currentValue = this._currentValue / newValue;
-        return this;
+        return new Divide(this._currentValue, newValue);
       default:
         throw new Error("Unsupported operation");
     }
   }
 
+  private execOperation(operation: "add" | "subtract" | "multiply" | "divide", newValue: number) {
+    const newOperation = this.createOperation(operation, newValue);
+    this._operations.push(newOperation);
+    this._currentValue = newOperation.operate();
+    return this;
+  }
+
+  add(newValue: number) {
+    return this.execOperation("add", newValue);
+  }
+
+  subtract(newValue: number) {
+    return this.execOperation("subtract", newValue);
+  }
+
+  multiplyBy(newValue: number) {
+    return this.execOperation("multiply", newValue);
+  }
+
+  divideBy(newValue: number) {
+    return this.execOperation("divide", newValue);
+  }
+
   printOperations() {
-    for (const operationObj of this._operations) {
-      switch (operationObj.operation) {
-        case "add":
-          console.log(`${operationObj.oldValue} plus ${operationObj.value}`);
-          break;
-        case "subtract":
-          console.log(`${operationObj.oldValue} minus ${operationObj.value}`);
-          break;
-        case "multiply":
-          console.log(`${operationObj.oldValue} multiplied by ${operationObj.value}`);
-          break;
-        case "divide":
-          console.log(`${operationObj.oldValue} divided by ${operationObj.value}`);
-          break;
-      }
-    }
+    this._operations.forEach((operation) => console.log(operation.toString()));
     console.log("-----------");
     console.log(`Total: ${this._currentValue}`);
   }
@@ -65,12 +130,6 @@ class Calculator {
 
 const calculator = new Calculator(0);
 
-calculator
-  .execute("add", 10)
-  .execute("add", 20)
-  .execute("subtract", 15)
-  .execute("multiply", 3)
-  .execute("divide", 2)
-  .printOperations();
+calculator.add(10).add(20).subtract(15).multiplyBy(3).divideBy(2).printOperations();
 
 export {};
